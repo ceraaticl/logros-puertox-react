79695991a2e806130a1c66e071cbed17687c9075
Nicolas Navarrete M <nikkolasnv1997@gmail.com>
2022-02-08 12:59:11 -0300

    config inicial del proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import "@material-tailwind/react/tailwind.css"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import Login from "views/Login"
+import Bills from "views/Bills"
+import Navbar from "components/Navbar"
 
 function App() {
+  console.log(localStorage.token)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App sans h-screen flex flex-col">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/facturas" element={<Bills />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
