762ba4edbac140bf8f5b62a169f07deb9ea1470a
Nicolas Navarrete M <nikkolasnv1997@gmail.com>
2022-03-02 18:02:52 -0300

    Cambios para producción

--- src/App.js
+++ src/App.js
@@ -8,13 +8,16 @@ import Bills from "views/Bills"
 import Navbar from "components/Navbar"
 import { useState } from "react"
 
+const baseName =
+  process.env.NODE_ENV === "production" ? "/" : "/nicolas/LogrosPuertoX/web"
+
 // rutas
 function App() {
   const [user, setUser] = useState()
 
   return (
     <div className="flex flex-col min-h-screen App sans">
-      <Router basename="/nicolas/LogrosPuertoX/web">
+      <Router basename={baseName}>
         <Navbar user={user} />
         <Routes>
           <Route exact path="/" element={<Login setUser={setUser} />} />
